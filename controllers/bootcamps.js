6e36aa4607b98940e56db776492b18cb13d2f0fb
mondmon <mohammed.osman@moniemo.com>
2020-03-21 18:09:04 +0200

    starting with custom error handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = async (req, res, next) => {
       .status(200)
       .json({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -31,13 +31,7 @@ exports.getBootcamp = async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    // res.status(400).json({ success: false });
-    next(
-      new ErrorResponse(
-        `Bootcamp not found with an id of ${res.params.id}`,
-        404
-      )
-    );
+    next(err);
   }
 };
 
@@ -49,7 +43,7 @@ exports.createBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.create(req.body);
     res.status(201).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -68,7 +62,7 @@ exports.updateBootcamp = async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    return res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -83,6 +77,6 @@ exports.deleteBootcamp = async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: {} });
   } catch (err) {
-    return res.status(400).json({ success: false });
+    next(err);
   }
 };
--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,20 @@
+const ErrorResponse = require("../utils/errorResponse");
+
 const errorHandler = (err, req, res, next) => {
-  //Log to console
+  let error = { ...err };
+
+  error.message = err.message;
+  //Log to console for development purposes
   console.log(err.stack.red);
+
+  //mongoose bad ObjectId
+  if (err.name === "CastError") {
+    const message = `No BootCamp found with the id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
   res
-    .status(err.statusCode || 500)
-    .json({ success: false, error: err.message || "Internal Server Error" });
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || "Internal Server Error" });
 };
 module.exports = errorHandler;
