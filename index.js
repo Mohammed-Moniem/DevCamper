259a6ac3ebfacdb59324a11b297857d5621821df
mondmon <mohammed.osman@moniemo.com>
2020-03-25 17:08:15 +0200

    Adding reviews functionality

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const path = require("path");
 
 //Import Route Files
 const auth = require("./routes/auth");
+const users = require("./routes/users");
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
 
@@ -40,6 +41,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //Mount routers
 app.use("/api/v1/auth", auth);
+app.use("/api/v1/users", users);
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
 app.use(errorHandler);
